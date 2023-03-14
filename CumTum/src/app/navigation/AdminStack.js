3259224d28685257366759ee9a0e988da27d47ce
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2023-03-14 14:18:45 +0700

    edit Router

--- CumTum/src/app/navigation/AdminStack.js
+++ CumTum/src/app/navigation/AdminStack.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Router from './Router';
+const Tab = createBottomTabNavigator();
 
 const AdminStack = () => {
   return (
-    <View>
-      <Text>AdminStack</Text>
-    </View>
-  )
-}
+    <Tab.Navigator>
+      <Tab.Screen
+        name={Router.Home}
+        component={ArticlesScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-export default AdminStack
+export default AdminStack;
--- CumTum/src/app/navigation/Router.js
+++ CumTum/src/app/navigation/Router.js
@@ -1,5 +1,31 @@
 const Router = {
-  Home: 'HOME',
-  Login: 'LOGIN',
+  /* Admin */
+  HOME_ADMIN: 'HomeAdmin',
+  DETAIL_CART_ADMIN:"DetailCartAdmin",
+  SHOW_BILL_ADMIN:"ShowBillAdmin",
+  MANGER:"Manager",
+  CRUD_MENU:"CrudMenu",
+  ADD_DISH:"AddDish",
+  EDIT_DISH:"EditDish",
+  UPDATE_DISH:"UpdateDish",
+  SUPPORT:"Support",
+  INCOME:"Income",
+  CHART:"Chart",
+  /* Customer */
+  SPLASH_SCREEN:"Splash",
+  LOGIN: 'Login',
+  UPDATE_INFO1:"UpdateInfo1",
+  OTP:"Otp",
+  HOME_CUSTOMER:"MenuMain",
+  CART_WITH_NO_ITEM:"CartWithNoItem",
+  CART_WITH_ITEM:"CartWithItems",
+  CART_CAN_DENY:"CartCanDeny",
+  PAYMENT:"Payment",
+  HISTORY_WITH_NO_ORDER:"HistoryOrderWithNoOder",
+  HISTORY_WITH_ITEMS:"HistoryOrderItems",
+  PROFILE:"Profile",
+  EDIT_PROFILE:"EditProfile",
+  UPLOAD_IMAGE:"UploadImage",
+
 };
 export default Router;
