2995258adb08974043f16a54d570e0e6a1f5498a
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2023-03-14 13:45:27 +0700

    update

---
+++ CumTum/src/app/navigation/AdminStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AdminStack = () => {
+  return (
+    <View>
+      <Text>AdminStack</Text>
+    </View>
+  )
+}
+
+export default AdminStack
