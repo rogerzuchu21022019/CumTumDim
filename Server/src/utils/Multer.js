0e95cb61ef6c8be41c91cf786650bd657df5ee96
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2023-03-15 21:54:01 +0700

    update

---
+++ Server/src/utils/Multer.js
@@ -0,0 +1,13 @@
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = `${Date.now()}_${Math.round(Math.random() * 1e9)}`;
+    cb(null, `${uniqueSuffix}_${file.originalname}.jpg`);
+  },
+});
+
+module.exports = multer({ storage: storage });
