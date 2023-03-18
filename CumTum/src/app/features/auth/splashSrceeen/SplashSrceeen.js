06cdecb22c4d861a31ceb4b388cb3b99f9ad75da
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2023-03-18 13:13:39 +0700

    fix auth folder

--- CumTum/src/app/features/auth/splashSrceeen/SplashSrceeen.js
+++ CumTum/src/app/features/auth/splashSrceeen/SplashSrceeen.js
@@ -1,29 +1,27 @@
-import { View, Text,Image } from 'react-native'
-import React, {useEffect} from 'react'
+import {View, Text, Image} from 'react-native';
+import React, {useEffect} from 'react';
 import Router from '../../../navigation/Router';
-import stylesplashSrceeen from './StyleSplashSrceeen'
-const SplashSrceeen = (props) => {
-     const { navigation } = props;
-     const nextScreen = () => {
-         navigation.replace(Router.LOGIN);
-     }
-     useEffect(() => {
-         setTimeout(nextScreen, 3000);
-     }, []);
+import stylesplashSrceeen from './StyleSplashSrceeen';
+const SplashSrceeen = props => {
+  const {navigation} = props;
+  const nextScreen = () => {
+    navigation.replace(Router.LOGIN);
+  };
+  useEffect(() => {
+    setTimeout(nextScreen, 3000);
+  }, []);
   return (
-   <View style={stylesplashSrceeen.container}>
-       <View style={stylesplashSrceeen.header}></View>
-       <View style={stylesplashSrceeen.body}>
-       <Image
-            style={stylesplashSrceeen.imageBody}
-            source={require('../../../../../../src/assets/image/logo.png')}
-          /> 
-       </View>
-       <View style={stylesplashSrceeen.foosters}></View>
+    <View style={stylesplashSrceeen.container}>
+      <View style={stylesplashSrceeen.header}></View>
+      <View style={stylesplashSrceeen.body}>
+        <Image
+          style={stylesplashSrceeen.imageBody}
+          source={require('../../../../assets//logo.png')}
+        />
+      </View>
+      <View style={stylesplashSrceeen.foosters}></View>
+    </View>
+  );
+};
 
-
-   </View>
-  )
-}
-
-export default SplashSrceeen
+export default SplashSrceeen;
