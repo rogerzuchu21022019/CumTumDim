f0321ef9f0307acf976225c854b0bd039b022986
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2023-03-18 13:28:41 +0700

    update

--- CumTum/src/app/features/auth/login/StyleLogin.js
+++ CumTum/src/app/features/auth/login/StyleLogin.js
@@ -2,71 +2,70 @@ const { StyleSheet } = require("react-native");
 
 const stylesLogin = StyleSheet.create({
     container: {
-        flex:10,
-        backgroundColor:"#373232"
+        flex: 10,
+        backgroundColor: "#373232"
     },
-    /*Header*/ 
+    /*Header*/
     header: {
-        flex:7,
+        flex: 6,
         backgroundColor: 'black',
         borderBottomLeftRadius: 20,
         borderBottomRightRadius: 20,
-        width:414,
-        height:513,
     },
-    Textheader:{
-        width:172,
-        height:49,
-        fontWeight:'900',
-        fontSize:32,
-        fontStyle:"normal",
-        lineHeight:39,
-        color:"white",
-        top:30,
-        left:130,
+    textHeader: {
+        fontWeight: '900',
+        fontSize: 22,
+        fontStyle: "normal",
+        lineHeight: 39,
+        color: "white",
+        textAlign: 'center',
+        paddingTop: 15,
+    },
+    viewImage: {
         alignItems: 'center',
-  justifyContent: 'center',
+        justifyContent: 'center',
 
     },
-    imageHeader:{
-        width:305,
-        height:294,
-        left:51,
-        top:50
+    imageHeader: {
+        width: 305,
+        height: 294,
+        marginTop: 30
     },
-    
-    /*Header*/ 
 
-    /*Body*/ 
+    /*Header*/
+
+    /*Body*/
     body: {
-        flex:4,
+        flex: 4,
         backgroundColor: '#373232',
-       
-    },
-    imagebody1:{
-        
-        width:167,
-        height:77,
-        marginLeft:100,
-        bottom:5,
-    },
-    imagebody2:{
-        width:39,
-        height:39,
-        marginLeft:42,
-        bottom:65,
-    },
-    TouchableOpacitybody:{
-        backgroundColor:"white",
-        width:262,
-        height:59,
-        left:76,
-        top:100,
-        borderRadius:30,
-    },
-     /*Body*/ 
 
-    foosters: {
+    },
+    imageBody1: {
+
+        width: 167,
+        height: 77,
+        marginLeft: 100,
+        bottom: 5,
+    },
+    imageBody2: {
+        width: 39,
+        height: 39,
+        marginLeft: 42,
+        bottom: 65,
+    },
+    touchAbleOpacityBody: {
+        backgroundColor: "white",
+        width: 262,
+        height: 59,
+        borderRadius: 30,
+    },
+    viewbtn: {
+        alignItems: 'center',
+        marginTop: 90
+    },
+    /*Body*/
+
+    footers: {
 
     },
 })
