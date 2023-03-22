88d046abb7d52fa2a0392cfbcf2a3e2844ee48e5
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2023-03-22 20:25:17 +0700

    update BottomTab ProfileTabs

--- CumTum/src/app/features/admin/screens/statistic/Statistic.js
+++ CumTum/src/app/features/admin/screens/statistic/Statistic.js
@@ -1,14 +1,14 @@
-import { Text, View, Image, ScrollView, FlatList } from 'react-native';
+import {Text, View, Image, ScrollView, FlatList} from 'react-native';
 import React from 'react';
 import styles from './StyleStatistic';
 import SafeKeyComponent from '../../../../components/safe_area/SafeKeyComponent';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import DATA from './Data';
 import ItemView from './ItemView';
-import { constants } from '../../../../shared/constants';
+import {constants} from '../../../../shared/constants';
 import FastImage from 'react-native-fast-image';
 import IconOcticons from 'react-native-vector-icons/Octicons';
-const Statistic = ({ navigation }) => {
+const Statistic = ({navigation}) => {
   return (
     <SafeKeyComponent>
       <View style={styles.container}>
@@ -29,17 +29,13 @@ const Statistic = ({ navigation }) => {
               />
             </View>
           </View>
-          <View style={styles.divideLine}></View>
         </View>
         <View style={styles.body}>
           <View style={styles.viewFlashList}>
             <FlatList
               data={DATA}
-
-              renderItem={({ item }) => (
-
+              renderItem={({item}) => (
                 <ItemView item={item} navigation={navigation} />
-
               )}
               keyExtractor={(item, index) => index.toString()}
             />
--- CumTum/src/app/navigation/CustomerStack.js
+++ CumTum/src/app/navigation/CustomerStack.js
@@ -128,7 +128,7 @@ const CustomerStack = () => {
 
       <Tab.Screen
         name={Router.PROFILE_TABS}
-        component={Profile}
+        component={ProfileTabs}
         options={{
           headerShown: false,
           tabBarIcon: ({ focused }) => (
@@ -150,7 +150,7 @@ const CustomerStack = () => {
                   lineHeight: 15,
                   color: focused ? constants.COLOR.YELLOW : constants.COLOR.WHITE
                 }}
-              >Thống kê</Text>
+              >Hồ sơ</Text>
             </View>
           )
         }}
