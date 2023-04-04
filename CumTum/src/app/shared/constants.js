eed51d3cf6a709e6572e6e15da5eb033cd62e7bf
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2023-04-04 09:45:19 +0700

    update

--- CumTum/src/app/shared/constants.js
+++ CumTum/src/app/shared/constants.js
@@ -12,7 +12,7 @@ export const constants = {
     GREEN: 'green',
   },
   BASE_URL: {
-    MAIN: 'http://192.168.1.10:3000/api',
+    MAIN: 'http://192.168.2.7:3000/api',
     SECOND: 'https://cumtumdim.becofoodstore.click/api',
   },
   SOCKET_URL: {
