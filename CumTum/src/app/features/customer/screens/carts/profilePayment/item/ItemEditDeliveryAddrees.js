a359ce4ccbd93295034f1d500ec2e9f7478dd483
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2023-04-16 19:37:26 +0700

    update

--- CumTum/src/app/features/customer/screens/carts/profilePayment/item/ItemEditDeliveryAddrees.js
+++ CumTum/src/app/features/customer/screens/carts/profilePayment/item/ItemEditDeliveryAddrees.js
@@ -1,52 +1,52 @@
-import {View, Text, TouchableOpacity,TextInput} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput} from 'react-native';
 import React from 'react';
-import styles from './stylesItemEditDeliveryAddress'
+import styles from './stylesItemEditDeliveryAddress';
 import SafeKeyComponent from '../../../../../../components/safe_area/SafeKeyComponent';
 const ItemEditDeliveryAddrees = ({item, navigation, index}) => {
   return (
     <SafeKeyComponent>
-     <View style={styles.groupAll}>
-            <View style={styles.item}>
-              <View style={styles.viewTitle}>
-                <Text style={styles.textTitle}>Họ và tên</Text>
-              </View>
-              <View style={styles.viewInput}>
-                <TextInput style={styles.textInput}>{item.name}</TextInput>
-              </View>
-            </View>
-            <View style={styles.item}>
-              <View style={styles.viewTitle}>
-                <Text style={styles.textTitle}>Số điện thoại</Text>
-              </View>
-              <View style={styles.viewInput}>
-                <TextInput style={styles.textInput}>{item.phone}</TextInput>
-              </View>
-            </View>
-            <View style={styles.item}>
-              <View style={styles.viewTitle}>
-                <Text style={styles.textTitle}>Phường</Text>
-              </View>
-              <View style={styles.viewInput}>
-                <TextInput style={styles.textInput}>{item.ward}</TextInput>
-              </View>
-            </View>
-            <View style={styles.item}>
-              <View style={styles.viewTitle}>
-                <Text style={styles.textTitle}>Đường</Text>
-              </View>
-              <View style={styles.viewInput}>
-                <TextInput style={styles.textInput}>{item.street}</TextInput>
-              </View>
-            </View>
-            <View style={styles.item}>
-              <View style={styles.viewTitle}>
-                <Text style={styles.textTitle}>Số nhà</Text>
-              </View>
-              <View style={styles.viewInput}>
-                <TextInput style={styles.textInput}>{item.houseNumber}</TextInput>
-              </View>
-            </View>
+      <View style={styles.groupAll}>
+        <View style={styles.item}>
+          <View style={styles.viewTitle}>
+            <Text style={styles.textTitle}>Họ và tên</Text>
           </View>
+          <View style={styles.viewInput}>
+            <TextInput style={styles.textInput}>{item.name}</TextInput>
+          </View>
+        </View>
+        <View style={styles.item}>
+          <View style={styles.viewTitle}>
+            <Text style={styles.textTitle}>Số điện thoại</Text>
+          </View>
+          <View style={styles.viewInput}>
+            <TextInput style={styles.textInput}>{item.phone}</TextInput>
+          </View>
+        </View>
+        <View style={styles.item}>
+          <View style={styles.viewTitle}>
+            <Text style={styles.textTitle}>Phường</Text>
+          </View>
+          <View style={styles.viewInput}>
+            <TextInput style={styles.textInput}>{item.ward}</TextInput>
+          </View>
+        </View>
+        <View style={styles.item}>
+          <View style={styles.viewTitle}>
+            <Text style={styles.textTitle}>Đường</Text>
+          </View>
+          <View style={styles.viewInput}>
+            <TextInput style={styles.textInput}>{item.street}</TextInput>
+          </View>
+        </View>
+        <View style={styles.item}>
+          <View style={styles.viewTitle}>
+            <Text style={styles.textTitle}>Số nhà</Text>
+          </View>
+          <View style={styles.viewInput}>
+            <TextInput style={styles.textInput}>{item.houseNumber}</TextInput>
+          </View>
+        </View>
+      </View>
     </SafeKeyComponent>
   );
 };
